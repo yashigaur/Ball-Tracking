f90442d24886bdeb8781050045b4c9114e779c22
Yashi Gaur <66491061+yashigaur@users.noreply.github.com>
2021-02-14 00:39:10 +0530

    Add files via upload

---
+++ Copy of CODE (1).py
@@ -0,0 +1,197 @@
+# USAGE
+# python ball_tracking.py --video ball_tracking_example.mp4
+# python ball_tracking.py
+
+# import the necessary packages
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+import RPi.GPIO as gpio
+import time
+
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+def forward(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def reverse(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(11, False)
+    gpio.output(13, False)
+    gpio.output(15, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def turn_left(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, False)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def turn_right(tf):
+    init()
+    gpio.output(7,True)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def pivot_left(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, False)
+    gpio.output(15, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def pivot_right(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(11, False)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+    help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+    help="max buffer size")
+args = vars(ap.parse_args())
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+pts = deque(maxlen=args["buffer"])
+
+# if a video path was not supplied, grab the reference
+# to the webcam
+if not args.get("video", False):
+    camera = cv2.VideoCapture(0)
+
+# otherwise, grab a reference to the video file
+else:
+    camera = cv2.VideoCapture(args["video"])
+
+# keep looping
+while True:
+    # grab the current frame
+    (grabbed, frame) = camera.read()
+
+    # if we are viewing a video and we did not grab a frame,
+    # then we have reached the end of the video
+    if args.get("video") and not grabbed:
+        break
+
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        #EDIT:
+        if int(x) > int(200) and int(x) < int(400):
+            middle = True
+            left = False
+            notleft = False
+            if radius < 30:
+                forward(0.05)
+
+            if radius > 60:
+                reverse(0.05)
+            
+
+        if int(x) > int(1) and int(x) < int(200):
+            left = True
+            middle = False
+            notleft = False
+            pivot_left(0.05)
+
+        if int(x) > int(400) and int(x) < int(600):
+            notleft = True
+            left = False
+            middle = False
+            pivot_right(0.05)
+
+        print ("middle: ", middle, " left: ", left, " right: ", notleft, radius)
+
+    # update the points queue
+    pts.appendleft(center)
+
+    # loop over the set of tracked points
+    for i in range(1, len(pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    # show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
+
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
